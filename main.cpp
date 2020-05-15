a2d1ebac474f6faa37e291a5b82c3c7bf74f1aeb
Ivan <iancurtis1507@mail.ru>
2020-05-15 12:26:03 +0300

    remember the cats

--- main.cpp
+++ main.cpp
@@ -1,67 +1,41 @@
-#include <iostream>
-using namespace std;
-void sort(int A[],int n){
-for(int i=0;i<n-1;i++){
-    for(int j=0;j<n-1;j++){
-        if(A[j]>A[j+1]){
-            int t=A[j];
-            A[j]=A[j+1];
-            A[j+1]=t;
-        }
-    }
-}
-}
-void sort_min(int A[],int n){
-for(int i=0;i<n-1;i++){
-    for(int j=0;j<n-1;j++){
-        if(A[j]<A[j+1]){
-            int t=A[j];
-            A[j]=A[j+1];
-            A[j+1]=t;
-        }
-    }
-}
-}
-void print(int A[],int n){
+ #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct Cat {
+    char *name;
+    unsigned int id;
+    double weight, length;
+    unsigned int mice_caught;
+};
+
+
+Cat* get_home_for_a_cats_pride(unsigned int n){
+Cat *cats=new Cat[n];
 for(int i=0;i<n;i++){
-    cout<<A[i]<<'\t';
+cats[i].name=new char[10];
 }
+return &cats[0];
 }
-int main()
-{
-    int n;
-    cin>>n;
-    int A[n];
-    bool F[n];
-    for(int i=0;i<n;i++){
-        cin>>A[i];
-        if(A[i]>0) F[i]=true;
-        else F[i]=false;
+void clear_home_of_a_cats_pride(Cat *cats, unsigned int n){
+for(int i=0;i<n;i++){
+    delete[] cats[i].name;
+}
+delete[] cats;
+}
+int main() {
+    unsigned int n;
+    cin >> n;
+    Cat *a = get_home_for_a_cats_pride(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].name >> a[i].weight >> a[i].length >> a[i].mice_caught;
+        a[i].id = i;
     }
-    for(int i=0;i<n-1;i++){
-       for(int j=0;j<n-i-1;j++){
-        if(A[j]<0&&A[j+1]<0){
-                if(A[j]<A[j+1]){
-                    int t=A[j];
-            A[j]=A[j+1];
-            A[j+1]=t;
-            print(A,n);
-            cout<<endl;
-                }
-            }
-            else if(A[j]>0&&A[j+1]>0){
-                if(A[j]>A[j+1]){
-                    int t=A[j];
-            A[j]=A[j+1];
-            A[j+1]=t;
-            print(A,n);
-            cout<<endl;
-       }
-    }}
-    }
-
-
-
-
+    for (int i = 0; i < n; i++)
+        cout << a[i].name << " ";
+    cout << endl;
+    clear_home_of_a_cats_pride(a, n);
+    return 0;
 }
 
