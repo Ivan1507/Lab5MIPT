457f59b6ac14ac9d04832205387983ca2764ca8e
Ivan <iancurtis1507@mail.ru>
2020-05-18 13:53:11 +0300

    Заботливый Фермер

--- main.cpp
+++ main.cpp
@@ -1,47 +1,22 @@
- #include <iostream>
-    using std::cin;
-    using std::cout;
-    using std::endl;
+#include <iostream>
+#include <math.h>
+using namespace std;
 
-    void my_slightly_less_dumb_reallocation(int** source, unsigned int n_old, unsigned int n_new);
-
-int main() {
-    unsigned int n, i;
-    cin >> n;
-    int *a = NULL;
-    my_slightly_less_dumb_reallocation(&a, 0, n / 2);
-    for (i = 0; i < n / 2; i++)
-        cin >> a[i];
-    my_slightly_less_dumb_reallocation(&a, n / 2, n);
-    for (; i < n; i++)
-        cin >> a[i];
-    int sum = 0;
-    for (i = 0; i < n; i++)
-        sum += a[i];
-    cout << sum << endl;
-    my_slightly_less_dumb_reallocation(&a, n, n / 2);
-    my_slightly_less_dumb_reallocation(&a, n / 2, 0);
-    my_slightly_less_dumb_reallocation(&a, 0, 0);
-    return 0;
+int main()
+{
+ int n,k;
+ cin>>n>>k;
+ int A[n];
+ int max=0;
+ for(int i=0;i<n;i++){
+    cin>>A[i];
+ }
+if(k>2){
+    max=min(A[n-1]-A[n/2],A[n/2]-A[0]);
 }
-void my_slightly_less_dumb_reallocation(int** source, unsigned int n_old, unsigned int n_new){
-    if(!(*source==0)&&(n_old<n_new)){
-       source=new int*[n_old];
-       delete[] source;
-       }
- delete[] source;
-    if(!(source==0&&n_old==0&&n_new==0)&&(n_old<n_new)){
-            int *nsource=new int[n_old];
-        for(int i=0;i<=n_old;i++){
-            source[i]=new int[n_old];
-        }
-    for(int i=0;i<=n_old;i++){
-        nsource[i]=*source[i];
-    }
+else max=A[n-1]-A[0];
+cout<<max;
 
-    }
+}
 
-     for(int i=0;i<n_old;i++){
-        delete source[i];
-     }
-    }
+
